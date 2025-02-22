1d020ad37ee16d897b7833c63f25ccf2ed137053
mohanhiscope <b.mohan1815@gmail.com>
2025-02-22 14:44:14 +0530

    h1

---
+++ js index.js
@@ -0,0 +1,8 @@
+console.log(`Hello`);
+//console.log(`i love pizza`);
+
+//window.alert(`this is an alert`);
+//window.alert(`i love pizza`);
+ 
+//document.getElementById("myH1").textContent = `Hello World`;
+//document.getElementById("myH2").textContent = `I love pizza`;
